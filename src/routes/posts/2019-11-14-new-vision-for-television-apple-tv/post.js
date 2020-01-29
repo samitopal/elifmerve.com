99b9b472231474c6b23d08b00ff8beb000a8b027
Sami Topal <samitopal@gmail.com>
2020-01-29 20:36:40 +0300

    .

---
+++ src/routes/posts/2019-11-14-new-vision-for-television-apple-tv/post.js
@@ -0,0 +1,7 @@
+export default {
+  title: 'A new vision for television: Apple TV+”',
+  tags: ['Apple', 'OTT', 'Digital Marketing', 'Disney Plus', 'Netflix'],
+  // eslint-disable-next-line max-len
+  spoiler: 'Comedies, Dramas, Thrillers, Documentaries and more\n',
+  getContent: () => import('./document.mdx'),
+};
