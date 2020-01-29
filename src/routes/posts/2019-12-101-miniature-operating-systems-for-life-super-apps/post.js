b411a4c510356404e389714c062a8c189dceaf70
Sami Topal <samitopal@gmail.com>
2020-01-29 20:31:08 +0300

    .

---
+++ src/routes/posts/2019-12-101-miniature-operating-systems-for-life-super-apps/post.js
@@ -0,0 +1,7 @@
+export default {
+  title: 'Miniature Operating Systems for Life: “Super Apps”',
+  tags: ['China', 'Super Apps', 'Wechat', 'Alipay', 'Gojek'],
+  // eslint-disable-next-line max-len
+  spoiler: 'Would you like an application that allows you to communicate, shop online, order cars, read books, play games, take out food, and pay for everything? In fact, these super apps already exist!',
+  getContent: () => import('./document.mdx'),
+};
